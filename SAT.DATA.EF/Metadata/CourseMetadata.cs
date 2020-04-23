3ca0749d4407fe11db273748db3102c3ec4af4a4
Wesley Lunnen <wesley.lunnen@gmail.com>
2020-04-23 10:40:39 -0500

    cont commit

---
+++ SAT.DATA.EF/Metadata/CourseMetadata.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SAT.DATA.EF.Metadata
+{
+    public class CourseMetadata
+    {
+        [Required(ErrorMessage = "*Required")]
+        [Display(Name = "Course ID")]
+        public int CourseId { get; set; }
+
+        [Required(ErrorMessage = "*Required")]
+        [Display(Name = "Course Name")]
+        public string CourseName { get; set; }
+
+        [Required(ErrorMessage = "*Required")]
+        [Display(Name = "Course Description")]
+        public string CourseDescription { get; set; }
+
+        [Required(ErrorMessage = "*Required")]
+        [Display(Name = "Credit Hours")]
+        public byte CreditHours { get; set; }
+
+        public string Curriculum { get; set; }
+
+        public string Notes { get; set; }
+
+        [Required(ErrorMessage = "*Required")]
+        [Display(Name = "Is Active?")]
+        public bool IsActive { get; set; }
+    }
+}
